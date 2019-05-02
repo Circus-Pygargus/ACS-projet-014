ea89f8aebe96a522ae790f854d1a9aed107ca601
Circus-Pygargus <richard.meuret@laposte.net>
2019-05-02 08:56:11 +0000

    ajout envoi formulaire en js

--- assets/js/form.js
+++ assets/js/form.js
@@ -12,27 +12,35 @@ var elements = form.elements;
 
 for (let item of elements) {
 
-  item.addEventListener('blur', function(){
+  item.addEventListener('blur', function () {
     // on retire la classe error au cas où
     this.classList.remove('error');
     // on chope le spanError contenu dans le label correspondant à l'input en question
     // span.msg-error : on chope le sapn ayant la classe qui nous intéresse
     spanErrMsg = document.querySelector('label[for="' + this.getAttribute('id') + '"] span.msg-error'); // span : on chope le premier span à l'intérieur du label
+    // ce if évite un message d'erreur avec le bouton du formulaire
+    if (spanErrMsg != null) {
+      // retire la classe d'erreur
       spanErrMsg.classList.remove('msg-error--show');
       if (!this.validity.valid) {
-      // ajoute la classe error
-      this.classList.add('error');
+        // ajoute la classe error
+        this.classList.add('error');
+        // colle la classe d'erreur
         spanErrMsg.classList.add('msg-error--show');
-      // annule l'envoi du formulaire
-      event.preventDefault();
+        // annule l'envoi du formulaire
+        event.preventDefault();
+      }
     }
   });
 }
 
 
 /* #### submit #### */
-form.addEventListener('submit', function(){
+form.addEventListener('submit', function () {
+  // prevent sending the form when click on submit button
+  event.preventDefault();
   var elements = form.elements;
+  var errorCounter = 0;
   for (let item of elements) {
     // le champs n'est pas valide
     /* ici on comprend que le novalidate sur le form en html n'empèche pas le testr de validation 
@@ -43,9 +51,54 @@ form.addEventListener('submit', function(){
       // on chope le spanError contenu dans le label correspondant à l'input dont on parle
       spanErrMsg = document.querySelector('label[for="' + item.getAttribute('id') + '"] span.msg-error'); // span : on chope le premier span à l'intérieur du label
       spanErrMsg.classList.add('msg-error--show');
-      // prevent sending the form when click on submit button
-      event.preventDefault();
     }
+  }
+  if (errorCounter == 0) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'assets/php/testAndSendForm.php');
+    var data = new FormData();
+    data.append('nom', document.querySelector('#nom').value);
+    data.append('adresse', document.querySelector('#adresse').value);
+    data.append('mail', document.querySelector('#mail').value);
+    data.append('message', document.querySelector('#message').value);
+    xhr.onload = function () {
+      console.log(this.responseText);
+    }
+    xhr.send(data);
   }
 });
 
+/* ################## tests ################## */
+// postForm('http://localhost:8080/')
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error))
+
+// function postForm(url) {
+//   const formData = new FormData(document.querySelector('form#myForm'))
+
+//   return fetch(url, {
+//     method: 'POST', 
+//     body: formData
+//   })
+//   .then(response => response.json())
+// }
+// var formData = new FormData(document.querySelector('#myForm'));
+// console.log(formData);
+
+
+// xhr est une techno inclue dans le navigateur
+// document.querySelector('#myForm').addEventListener('submit', function(){
+//   event.preventDefault();
+
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('POST', 'assets/php/testAndSendForm.php');
+//   var data = new FormData();
+//   data.append('nom',document.querySelector('#nom').value);
+//   data.append('adresse', document.querySelector('#adresse').value);
+//   data.append('mail', document.querySelector('#mail').value);
+//   data.append('message', document.querySelector('#message').value);
+//   xhr.onload = function(){
+//     console.log(this.responseText);
+//   }
+//   xhr.send(data);
+// });
---
+++ assets/php/testAndSendForm.php
@@ -0,0 +1,10 @@
+<?php
+
+$nom = $_POST['nom'];
+$adresse = $_POST['adresse'];
+$mail = $_POST['mail'];
+$message = $_POST['message'];
+echo $nom . ' ' . $adresse . ' ' . $mail . ' ' . $message;
+// echo 'coucou';
+
+?>
